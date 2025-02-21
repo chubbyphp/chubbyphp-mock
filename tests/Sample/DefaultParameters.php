56c10e6012d91d41690b582b925c36b8bf0b1bab
Dominik Zogg <dominik.zogg@hotmail.com>
2025-02-21 20:58:12 +0100

    Add  public static function staticDefaultParameters( to see if get mocked as well

--- tests/Sample/DefaultParameters.php
+++ tests/Sample/DefaultParameters.php
@@ -47,4 +47,40 @@ class DefaultParameters
         // sample
         Sample $sample = new Sample('name', 'value'),
     ): void {}
+
+    public static function staticDefaultParameters(
+        // null
+        null $null = null,
+        null $nullWithCustomConst = self::NULL,
+        // boolean
+        bool $booleanTrue = true,
+        bool $booleanFalse = false,
+        ?bool $optionalBooleanTrue = null,
+        bool $booleanWithConst = ZEND_THREAD_SAFE,
+        bool $booleanWithCustomConst = self::BOOLEAN,
+        // int
+        int $int = 42,
+        ?int $optionalInt = null,
+        int $intWithConst = PHP_INT_MIN,
+        int $intWithCustomConst = self::INT,
+        // float
+        float $float = 3.14159,
+        ?float $optionalFloat = null,
+        float $floatWithConst = PHP_FLOAT_MIN,
+        float $floatWithCustomConst = self::FLOAT,
+        // string
+        string $string = 'string',
+        ?string $optionalString = null,
+        string $stringWithConst = PHP_EOL,
+        string $stringWithCustomConst = self::STRING,
+        // array
+        array $array = ['null' => self::NULL, 'boolean' => self::BOOLEAN, 'int' => self::INT, 'float' => self::FLOAT, 'string' => self::STRING],
+        ?array $optionalArray = null,
+        // datetime
+        \DateTimeImmutable $dateTimeImmutable = new \DateTimeImmutable('2025-02-16T00:25:30+01:00', new \DateTimeZone('Europe/Zurich')),
+        // iterator
+        \ArrayIterator $arrayIterator = new \ArrayIterator(['null' => self::NULL, 'boolean' => self::BOOLEAN, 'int' => self::INT, 'float' => self::FLOAT, 'string' => self::STRING]),
+        // sample
+        Sample $sample = new Sample('name', 'value'),
+    ): void {}
 }
